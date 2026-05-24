5d5971839c4ca06a421c3aa6ffc5f1b75ddcfb4a
Jhonathan <jhonathandasilva2002@gmail.com>
2026-05-24 16:38:50 -0300

    fixed: getReactNativePersistence) is not a function

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,8 +3,7 @@ import { initializeApp } from 'firebase/app'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import AsyncStorage from '@react-native-async-storage/async-storage'
-import { getReactNativePersistence, initializeAuth } from 'firebase/auth'
+import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
@@ -21,9 +20,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 // auth
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage)
-})
+export const auth = getAuth(app)
 
 // db
 export const firestore = getFirestore(app)
